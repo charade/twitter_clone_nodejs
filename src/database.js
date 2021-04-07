59dbfc5b25896d1c4f16cd914a2d7bd0cb6347be
charade <66742561+charade@users.noreply.github.com>
2021-04-07 14:38:27 +0200

    Update database.js

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const database = mysql.createConnection({
    host : "localhost",
    user : "root",
-   password : "Kingsihanouk@90",
+   password : "password",
    database : "twitter_clone_nodejs"
 })
 
